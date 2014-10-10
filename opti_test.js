707874a3d67073fae3002d8cbfa574abb1b1f011
Amanpreet Singh <apsdehal@gmail.com>
2014-10-10 18:13:47 +0530

    Adds more tests for opti

--- opti_test.js
+++ opti_test.js
@@ -31,4 +31,51 @@ describe("Tests for registry", function () {
 		expect(registry.maxLength).toEqual(2000);
 	});
 
+	// testing all the function existence
+	it("should have a function store, lookup, remove, clear", function () {
+		expect(registry.store).toBeDefined();
+		expect(typeof registry.store).toBe('function');
+		expect(registry.lookup).toBeDefined();
+		expect(typeof registry.lookup).toBe('function');
+		expect(registry.remove).toBeDefined();
+		expect(typeof registry.remove).toBe('function');
+		expect(registry.clear).toBeDefined();
+		expect(typeof registry.clear).toBe('function');
+	});
+
+	// Testing store function
+	it("function store should work", function () {
+		var reg = registry.store(model);
+		expect(registry.cache['model1']).toBeDefined();
+		expect(typeof registry.cache['model1']).toBe('object');
+		expect(reg).toEqual(registry);
+		expect(Object.keys(registry.cache).length).toEqual(1);
+	});
+
+	// Testing lookup function
+	it("function lookup should work", function () {
+		registry.store(model);
+		var lookuped = registry.lookup(1, 'model');
+		expect(typeof lookuped).toBe('object');
+		expect(lookuped.type).toEqual('model');
+		expect(lookuped.id).toEqual(1);
+		expect(typeof lookuped.getId).toBe('function');
+	});
+
+	// Testing remove function
+	it("function remove should work", function () {
+		registry.store(model);
+		var isRemoved = registry.remove(1, 'model');
+		expect(isRemoved).toBeTruthy();
+		var lookuped = registry.lookup(1, 'model');
+		expect(lookuped).toBeUndefined();
+		expect(Object.keys(registry.cache).length).toBe(0);
+	});
+
+	// Testing clear function
+	it("function clear should work", function () {
+		registry.store(model);
+		registry.clear();
+		expect(Object.keys(registry.cache).length).toBe(0);
+	});
 });
